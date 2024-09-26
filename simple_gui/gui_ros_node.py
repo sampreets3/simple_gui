8b849bb2c4ad65dd816668dc6c1bb24caf485c96
Sampreet SARKAR <sampreets3@gmail.com>
2024-09-26 15:05:44 +0200

    Added the timer callback to update robot state

--- simple_gui/gui_ros_node.py
+++ simple_gui/gui_ros_node.py
@@ -1,6 +1,8 @@
 from rclpy.node import Node
+import numpy as np
 
 from example_interfaces.msg import Int32
+from example_interfaces.srv import AddTwoInts
 
 from PyQt5 import QtGui
 
@@ -16,8 +18,12 @@ class MyGuiNode(Node):
         self._btn_publisher = self.create_publisher(Int32, '/chatter', 10)
         self._subscriber    = self.create_subscription(Int32, '/chatter', self.subscriber_callback, 1)
 
+        # Define ROS 2 timers here
+        self._timer         = self.create_timer(1.0, self.timer_callback)
+
         # Define internal member variables
         self._count         = 0
+        self._robot_state   = [0.0, 0.0, 0.0]    # Store the robot's state [x, y, theta]
 
         # Sanity check
         self.get_logger().info("Node set up properly.")
@@ -48,5 +54,24 @@ class MyGuiNode(Node):
         temp = msg.data
         self.get_logger().info(f"[Subscriber] I heard: {temp}")
         self.ui.label_subscriber.setText(f"{msg.data}")
+
+    def timer_callback(self): 
+        """
+            This timer will be used to update the information about the
+            robot state. We will resort to publishing random numbers for
+            the time being. 
+
+            TODO    Listen to the odometry topic and update the values
+            accordingly.
+        """
+        self._robot_state[0] = np.random.random()
+        self._robot_state[1] = np.random.random()
+        self._robot_state[2] = np.random.random()
+
+        self.ui.robot_state_x.setText(f'{self._robot_state[0]:.4f}')
+        self.ui.robot_state_y.setText(f'{self._robot_state[1]:.4f}')
+        self.ui.robot_state_theta.setText(f'{self._robot_state[2]:.4f}')
+
+        self.get_logger().info(f'Robot state: [x: {self._robot_state[0] :.4f}, y: {self._robot_state[1]:.4f}, theta: {self._robot_state[2]:.4f}]')
 
 

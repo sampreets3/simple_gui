3f36af900f72363d3e7cc2c4f41a463dcd7b175f
Sampreet SARKAR <sampreets3@gmail.com>
2024-10-02 11:17:25 +0200

    Added launch file to start all necessary nodes

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
+import os
 from setuptools import setup
+from glob import glob
 
 package_name = 'simple_gui'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     py_modules=['simple_gui.simple_gui'],
     maintainer='Sampreet SARKAR',
-    maintainer_email='sampreet.sarkar@cea.fr',
+    maintainer_email='sampreets3@gmail.com',
     description='Simple GUI demo application for ROS2 using PyQt5',
     license='MIT',
     tests_require=['pytest'],
